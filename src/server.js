ed48a26d9627d9d93cd6e2ab8f499a4917509868
raghav-webseeder <mishra.raghav@webseeder.tech>
2025-11-24 12:19:34 +0530

    Added Cors URL

--- src/server.js
+++ src/server.js
@@ -23,7 +23,29 @@ const app = express();
 connectDatabase();
 
 app.use(helmet());
-app.use(cors());
+// CORS Allowed Origins
+const allowedOrigins = [
+  "https://crockery-e-com-dashboard.netlify.app",
+  "https://tanariri-frontend.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS: " + origin));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
